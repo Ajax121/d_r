be02fab0e5e1569f67eccdf0a2204441eea263fc
Arjun <arjunharidas@hotmail.com>
2022-02-23 15:49:44 +0100

    s

--- test.py
+++ test.py
@@ -3,6 +3,5 @@ import pandas as pd
 from recommender import random_recomm
 def test_recomm():
     """test"""
-    movies = pd.read_csv('data/movies.csv')
     recom = random_recomm()
     assert len(recom)==10
