5259201af8746ed90ac1c4502b1093edff1e81e2
Arjun <arjunharidas@hotmail.com>
2022-02-23 15:19:25 +0100

    s

--- recommender.py
+++ recommender.py
@@ -1,9 +1,9 @@
 """sdsd"""
 import pandas as pd
 def random_recomm(k=10):
-	"""
-	ads
-	"""
-	d_f = pd.read_csv('data/movies.csv')
-	return d_f['title'].sample(k)
+    """
+    ads
+    """
+    d_f = pd.read_csv('data/movies.csv')
+    return d_f['title'].sample(k)
 print(random_recomm())
